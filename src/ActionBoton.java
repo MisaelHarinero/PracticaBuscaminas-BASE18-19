e66588c7379f1d2f895eec6390061de196327a79
Misael Harinero <missigamer@gmail.com>
2018-10-24 19:13:42 +0200

    Cambio de Autor
    Cambios de Autor y algunos cambios que ha hecho al exportar el proyecto del repositorio

--- src/ActionBoton.java
+++ src/ActionBoton.java
@@ -6,17 +6,17 @@ import java.awt.event.ActionListener;
  * De alguna manera tendrá que poder acceder a la ventana principal.
  * Se puede lograr pasando en el constructor la referencia a la ventana.
  * Recuerda que desde la ventana, se puede acceder a la variable de tipo ControlJuego
- * @author jesusredondogarcia
+ * @author misaelHarinero
  **
  */
 public class ActionBoton implements ActionListener{
 
-	
+
 
 	public ActionBoton() {
 		//TODO
 	}
-	
+
 	/**
 	 *Acción que ocurrirá cuando pulsamos uno de los botones.
 	 */
--- src/Principal.java
+++ src/Principal.java
@@ -2,7 +2,7 @@ import java.awt.EventQueue;
 
 /**
  * Clase principal
- * @author jesusredondogarcia
+ * @author misaelHarinero
  *
  */
 public class Principal {
